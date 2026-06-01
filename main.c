3af16bb0d2e21a88ff6c03693843e20d8ed40c03
Afonsinho22 <afonsos2009ferreira@gmail.com>
2026-06-01 14:43:54 +0100

    Update: pagina inicial do programa + comentarios

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <locale.h>
 
 #define LIMITE_USERNAME 50
 
@@ -10,13 +11,46 @@ void pedir_username(char nome[]){
     printf("Antes de continuar, introduz um username: ");
     nome[strcspn(nome, "\n")]='\0';
     fgets(nome, LIMITE_USERNAME, stdin);
-
-    printf("Nome do user: %s", nome);
 }
 
+int menu(){
+    int opcao_user_main;
+    printf("Para continuar, escolhe uma opção:\n");
+    printf("1- Jogar");
+    printf("0- Sair");
+    printf("Opção -> ");
+    scanf("%d", &opcao_user_main);
+
+    return opcao_user_main;
+}
+
+
 int main(){
+    //UTF-8
+    setlocale(LC_ALL, "pt_PT.UTF-8");
+    
+    //variaveis
     char username[LIMITE_USERNAME];
+    int opcao_inicial_user;
+
+    //Solicitacao do username
     pedir_username(username);
+
+    //página inicial do programa
+    printf("Olá %s! Bem-vindo ao MasterMind!\n", username);
+    printf("Neste jogo, terás que adivinhar um número de 4 dígitos sorteado aleatóriamente. Os números podem repetidos.\n");
+    opcao_inicial_user=menu();
+
+
+
+
+
+
+
+
+    //Testes:
+    
+    //printf("Numero: %d", opcao_inicial_user);
 
     return 0;
 }
