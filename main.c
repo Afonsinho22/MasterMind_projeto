0e6c6dcf514558f19f539cae49d92bb814ec0868
mateusgneves2010 <mateusjogadordaps@gmail.com>
2026-06-05 19:45:50 +0000

    Update: Fix erros gerarCombinacao e modificação do menu

--- main.c
+++ main.c
@@ -3,207 +3,227 @@
 #include <time.h>
 #include <string.h>
 #include <locale.h>
+#include <ctype.h>
 
 #define LIMITE_USERNAME 50
 #define TAMANHO_NUMEROS 4
 #define NUMERO_MINIMO 1
 #define NUMERO_MAXIMO 6
+#define MAX_TENTATIVAS 10
 
-typedef enum{
-    NO_MENU,
-    EM_CURSO,
-    VITORIA,
-    DERROTA,
-    DESISTIU
-}Estados;
-
-typedef struct{
-    int maximo_tentatias;
-    int tentativas_user_dadas;
-}Verificacao_tentativas;
-
-typedef struct{
-    int num_user[TAMANHO_NUMEROS];
-    int certos_posicao_certa;
-    int certos_posicao_errada;
-}Posicao_numeros;
-
-
-
-Estados estado_jogo=NO_MENU;
 char username[LIMITE_USERNAME];
 
-void pedir_username(char nome[]){
+typedef struct
+{
+    int certos_posicao_certa;
+    int certos_posicao_errada;
+} Posicao_numeros;
+
+void pedir_username(char nome[])
+{
     printf("Olá!\n");
     printf("Antes de continuar, introduz um username: ");
+
     fgets(nome, LIMITE_USERNAME, stdin);
-    nome[strcspn(nome, "\n")]='\0';
+    nome[strcspn(nome, "\n")] = '\0';
 }
 
-int menu(){
-    int opcao_user_main;
-    printf("Para continuar, escolhe uma opção:\n");
-    printf("1- Jogar\n");
-    printf("0- Sair\n");
+int menu()
+{
+    int opcao;
+
+    printf("\n=== MASTERMIND ===\n");
+    printf("1 - Jogar\n");
+    printf("0 - Sair\n");
     printf("Opção -> ");
-    scanf("%d", &opcao_user_main);
 
-    return opcao_user_main;
+    scanf("%d", &opcao);
+
+    while(getchar() != '\n');
+
+    return opcao;
 }
-/*/
-neste momento tens 33 tentativas
-(introduz 0 para sair)
-> 0
-Erraste ahah!
-neste momento tens 32 tentativas
-> 6789
-/*/
 
-void gerarCombinacao(int combinacao[]){
+void gerarCombinacao(int combinacao[])
+{
     int i, j;
     int numero;
     int repetido;
 
-    for(i = 0; i < TAMANHO_NUMEROS; i++){
-
-        do{
+    for(i = 0; i < TAMANHO_NUMEROS; i++)
+    {
+        do
+        {
             numero = rand() % 6 + 1;
-
             repetido = 0;
 
-            for(j = 0; j < i; j++){
-                if(combinacao[j] == numero){
+            for(j = 0; j < i; j++)
+            {
+                if(combinacao[j] == numero)
+                {
                     repetido = 1;
                     break;
                 }
             }
 
-        }while(repetido);
+        } while(repetido);
 
         combinacao[i] = numero;
     }
 }
 
+int pedir_palpite(int palpite[])
+{
+    char entrada[100];
 
-int pedir_palpite(int tentativas_user, int ciclo_jogo){
-    int opcao_tentativas;
-    int palpite_utilizador[TAMANHO_NUMEROS];
-    int numero_utilizador;
-    printf("Neste momento, tens %d tentativas.\n", tentativas_user);
-    printf("Escreve %d números, de %d até %d ou 0 para para desistires: ", TAMANHO_NUMEROS, NUMERO_MINIMO, NUMERO_MAXIMO);
-    scanf("%d", &numero_utilizador);
-    if(numero_utilizador==0){
-        printf("É uma pena teres abandonado o jogo a meio...\n");
-        printf("Para a próxima corre melhor.");
-        return 0;
-    }else{
-        for(int i=TAMANHO_NUMEROS-1; i>=0; i--){
-            palpite_utilizador[i]=numero_utilizador%10;
-            numero_utilizador=numero_utilizador/10;
+    while(1)
+    {
+        printf("\nIntroduz 4 números entre 1 e 6 (ou 0 para desistir): ");
+        fgets(entrada, sizeof(entrada), stdin);
+        entrada[strcspn(entrada, "\n")] = '\0';
+        if(strcmp(entrada, "0") == 0)
+        {
+            return 0;
         }
-        return *palpite_utilizador;
-        return 1;
-    }
-
-}   
-
-int verificarCombinacao(int combinacao[], int palpite[], Posicao_numeros *calc_posicoes){
-    calc_posicoes->certos_posicao_certa=0;
-    calc_posicoes->certos_posicao_errada=0;
-
-    if(combinacao==palpite){
-        printf("\nParabéns %s!\n", username);
-        printf("Acertaste na combinação!\n");
-        
-    }else{
-        for(int i = 0; i < TAMANHO_NUMEROS; i++){
-            if(palpite[i]<1 && palpite[i]>6){
-                printf("Erro: O número inserido não está entre 1 e 6\n");
-                printf("Vê se não voltes a cometer este erro.");
+        if(strlen(entrada) != 4)
+        {
+            printf("Erro: tens de introduzir exatamente 4 dígitos.\n");
+            continue;
+        }
+        int valido = 1;
+        for(int i = 0; i < TAMANHO_NUMEROS; i++)
+        {
+            if(!isdigit(entrada[i]))
+            {
+                valido = 0;
                 break;
             }
-        for(int i = 0; i < TAMANHO_NUMEROS; i++){
-            calc_posicoes->num_user[i]=palpite[i];
-            if(combinacao[i] == palpite[i]){
-                calc_posicoes->certos_posicao_certa++;
-                return 0;
-            }else if(combinacao[i]!=palpite[i]){
-                for(int j=0; j<TAMANHO_NUMEROS; j++){
-                    if(j!=i&&palpite[i]==combinacao[j]){
-                        calc_posicoes->certos_posicao_errada++;
-                        break;
-                    }
-                }
-                printf("Não acertaste no número!\n");
-                printf("Tens %d números no sítio certos.\n", calc_posicoes->certos_posicao_certa);
-                printf("Tens %d números na sítio errado.\n", calc_posicoes->certos_posicao_errada);
-            }
-            else{
-                printf("Ocurreu um erro inesperado ou não cumpriste os requesitos.");
+            palpite[i] = entrada[i] - '0';
+            if(palpite[i] < 1 || palpite[i] > 6)
+            {
+                valido = 0;
+                break;
             }
         }
 
-    }
-    
+        if(!valido)
+        {
+            printf("Erro: só são permitidos números entre 1 e 6.\n");
+            continue;
+        }
         return 1;
     }
-}  
-
-
-/*/
-for(int i=0; i<TAMANHO_NUMEROS; i++){
-            if(palpite_utilizador>0){
-                if(palpite_utilizador>=NUMERO_MINIMO && palpite_utilizador<=NUMERO_MAXIMO){
-                    return 
-                }
-            }
 }
-/*/
 
-int main(){
-    //UTF-8
-    setlocale(LC_ALL, "pt_PT.UTF-8");
-        
-    //variaveis
-    int opcao_inicial_user;
-    int ciclo_principal=1;
+int verificarCombinacao(int combinacao[],
+                        int palpite[],
+                        Posicao_numeros *resultado)
+{
+    resultado->certos_posicao_certa = 0;
+    resultado->certos_posicao_errada = 0;
 
-    //Solicitacao do username
-    pedir_username(username);
-
-    //página inicial do programa
-    printf("Olá %s! Bem-vindo ao MasterMind!\n", username);
-    printf("Neste jogo, terás que adivinhar um número de 4 dígitos sorteado aleatóriamente. Os números podem repetidos.\n");
-    opcao_inicial_user=menu();
-
-    //motor do jogo
-    while(ciclo_principal==1){
-        if(opcao_inicial_user==1){
-            //resto do código
-            int combinacao[TAMANHO_NUMEROS];
-            int dados_palpite=pedir_palpite(10, ciclo_principal);
-            if(dados_palpite==0){
-                break;
+    for(int i = 0; i < TAMANHO_NUMEROS; i++)
+    {
+        if(combinacao[i] == palpite[i])
+        {
+            resultado->certos_posicao_certa++;
+        }
+        else
+        {
+            for(int j = 0; j < TAMANHO_NUMEROS; j++)
+            {
+                if(i != j && palpite[i] == combinacao[j])
+                {
+                    resultado->certos_posicao_errada++;
+                    break;
+                }
             }
-            
-
-            //printf("A elaborar o programa. Espera por favor");
-            //break;
-        } else if(opcao_inicial_user==0){
-            printf("Obrigado por teres entrado no jogo. É uma pena teres desistido logo do jogo.\n");
-            printf("Até breve!");
-            break;
-        }else{
-            printf("A opção que introduziste está inválida.");
-            opcao_inicial_user=menu();
         }
     }
 
+    printf("\nResultado:\n");
+    printf("Números na posição certa: %d\n",
+           resultado->certos_posicao_certa);
 
+    printf("Números certos na posição errada: %d\n",
+           resultado->certos_posicao_errada);
 
+    if(resultado->certos_posicao_certa == TAMANHO_NUMEROS)
+    {
+        return 1;
+    }
+    return 0;
+}
 
-    //Testes:
-    //printf("Numero: %d", opcao_inicial_user);
+int main()
+{
+    setlocale(LC_ALL, "");
+    srand(time(NULL));
+    pedir_username(username);
+    printf("\nOlá %s!\n", username);
+    printf("Bem-vindo ao MasterMind!\n");
+    printf("Tens de adivinhar uma combinação de 4 números.\n");
+    printf("Cada número está entre 1 e 6.\n");
+    printf("Os números da combinação não se repetem.\n");
 
+    int opcao = menu();
+    if(opcao == 0)
+    {
+        printf("Até breve!\n");
+        return 0;
+    }
+    if(opcao != 1)
+    {
+        printf("Opção inválida.\n");
+        return 0;
+    }
+
+    int combinacao[TAMANHO_NUMEROS];
+    int palpite[TAMANHO_NUMEROS];
+    Posicao_numeros resultado;
+    gerarCombinacao(combinacao);
+    int tentativas = MAX_TENTATIVAS;
+    while(tentativas > 0)
+    {
+        printf("\n=====================================\n");
+        printf("Tentativas restantes: %d\n", tentativas);
+        printf("=====================================\n");
+        int estado = pedir_palpite(palpite);
+        if(estado == 0)
+        {
+            printf("\nDesististe do jogo.\n");
+            printf("A combinação era: ");
+
+            for(int i = 0; i < TAMANHO_NUMEROS; i++)
+            {
+                printf("%d", combinacao[i]);
+            }
+
+            printf("\n");
+            break;
+        }
+        if(verificarCombinacao(combinacao,
+                               palpite,
+                               &resultado))
+        {
+            printf("\nParabéns %s!\n", username);
+            printf("Acertaste na combinação!\n");
+            break;
+        }
+        tentativas--;
+        if(tentativas == 0)
+        {
+            printf("\nFicaste sem tentativas!\n");
+            printf("A combinação correta era: ");
+
+            for(int i = 0; i < TAMANHO_NUMEROS; i++)
+            {
+                printf("%d", combinacao[i]);
+            }
+
+            printf("\n");
+            break;
+        }
+    }
     return 0;
 }
